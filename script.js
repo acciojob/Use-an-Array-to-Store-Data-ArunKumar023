8f55490c2b47b2c4453a77e449849eaf739e47e2
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-19 15:15:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
+
 let yourArray = ['hello', 42, true, null, 'world'];
 console.log(yourArray.length);
 // logs 5
