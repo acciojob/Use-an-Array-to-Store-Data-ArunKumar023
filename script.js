1bee27ebec3005359f404b15bb419554a6a7d7da
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-30 07:12:43 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-const list = document.getElementById("my-list");
-for(let i=0; i<yourArray.length; i++){
-  let listItem = document.createElement("li");
-  listItem.innerHTML = yourArray[i];
-  list.appendChild(listItem);
-}
