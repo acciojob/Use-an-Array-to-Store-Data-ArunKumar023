136cefd34aabb32dad5baa0979e48661850ea538
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-30 07:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let yourArray = ['hello', 4, true, null];
+const list = document.getElementById("my-list");
+for(let i=0; i<yourArray.length; i++){
+  let listItem = document.createElement("li");
+  listItem.innerHTML = yourArray[i];
+  list.appendChild(listItem);
+}
