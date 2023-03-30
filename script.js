d9394eefb7f404b9d19f00019a3b7078512fa51d
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-30 06:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
- let yourArray = ["hello", 42, true, null, {name: "John"}];
-
-    // Access the array length property and display it
-    let length = yourArray.length;
-    console.log("Length:", length);
-
-    // Add each element of the array to an unordered list
-    let list = document.getElementById("my-list");
-    yourArray.forEach(function(item) {
-      let li = document.createElement("li");
-      li.textContent = item;
-      list.appendChild(li);
-    });
+let yourArray = ['hello', 123, true, null, 'world'];
+console.log(yourArray.length);
+// logs 5
